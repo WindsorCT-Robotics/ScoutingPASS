0622a74c8f983677324a25a64e6341c54fbb21be
Elijah Jubrey <elijahjubrey@gmail.com>
2026-03-25 08:20:43 -0400

    Update for 2026cthar

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -16,7 +16,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026mawne",
+      "defaultValue": "2026cthar",
       "required": "true"
     },
     { "name": "Match Type",
