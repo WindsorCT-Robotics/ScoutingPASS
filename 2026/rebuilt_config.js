47915b91f1cfa18b725b2a11a0d595a65b5d86a5
Elijah Jubrey <elijahjubrey@gmail.com>
2026-04-13 15:28:43 -0400

    update to burns division, 1

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -16,7 +16,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026necmp",
+      "defaultValue": "2026necmp1",
       "required": "true"
     },
     { "name": "Match Type",
